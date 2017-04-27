4c891b8e068181e89d9fdfabbfc0e63ac65b7560
Josh <jv15626@my.bristol.ac.uk>
2017-04-27 22:01:41 +0100

    removed sectors :[ whyyy

--- CW2/test.py
+++ CW2/test.py
@@ -15,6 +15,10 @@ from sklearn import neighbors, datasets
 
 #%matplotlib inline
 
+def kimeans(Data, NClusters):
+    km = KMeans(NClusters, n_init=100)
+    fitted = km.fit(Data)
+    return (fitted.cluster_centers_, fitted.labels_, fitted.inertia_)
 
 
 def plotmatrix(Matrix):
@@ -26,7 +30,6 @@ def plotmatrix(Matrix):
             ax = fig.add_subplot( c, c, plotID )
             ax.scatter( Matrix[:,i], Matrix[:,j] )
             plotID += 1
-    print('about to show')
     plt.show()
 
 
@@ -48,6 +51,10 @@ def read(char_name):
 def slice(old_matrix):
     factor     = old_matrix.shape[0]/2
     new_matrix = old_matrix[:factor,:]
+    for i in range(new_matrix.shape[0]):
+        for j in range(new_matrix.shape[1]):
+            if (np.absolute(new_matrix[i][j]) < 10000):
+                new_matrix[i][j] = 0
     return new_matrix
 
 
@@ -92,21 +99,25 @@ def spectralRegion(data):
             if (mag != 0):
                 n = center[0] - i
                 m = center[1] - j
-                if (m != 0):
-                    if (( math.tan(n/m) >= (3*math.pi/8) and  math.tan(n/m) <= (math.pi/2)) or
-                        ( math.tan(n/m) <=    math.pi/8)):
-                            feature1.append(mag)
+                if (i <= center[0]):
+                    feature1.append(mag)
+                else:
+                    feature2.append(mag)
+                #if (m != 0):
+                    ##if (( math.tan(n/m) >= (3*math.pi/8) and  math.tan(n/m) <= (math.pi/2)) or
+                    ##    ( math.tan(n/m) <=    math.pi/8)):
+                    ##        feature1.append(mag)
 
-                    if ( (math.tan(n/m) <= (3*math.pi/8)) and  math.tan(n/m) >= (math.pi/8)):
-                            feature2.append(mag)
+                    ##if ( (math.tan(n/m) <= (3*math.pi/8)) and  math.tan(n/m) >= (math.pi/8)):
+                    ##        feature2.append(mag)
 
-                if (n != 0):
-                    if ( (math.tan(m/n) >= (3*math.pi/8) and  math.tan(m/n) <= (math.pi/2)) or
-                         (math.tan(m/n) <=    math.pi/8)):
-                            feature1.append(mag)
+                #if (n != 0):
+                #    if ( (math.tan(m/n) >= (3*math.pi/8) and  math.tan(m/n) <= (math.pi/2)) or
+                #         (math.tan(m/n) <=    math.pi/8)):
+                #            feature1.append(mag)
 
-                    if (( math.tan(m/n) <= (3*math.pi/8)) and  math.tan(m/n) >= (math.pi/8)):
-                            feature2.append(mag)
+                #    if (( math.tan(m/n) <= (3*math.pi/8)) and  math.tan(m/n) >= (math.pi/8)):
+                #            feature2.append(mag)
 
     f = list(map(lambda x, y: [x,y], feature1, feature2))
     features = np.matrix(f)
@@ -133,46 +144,73 @@ def normalise(matrix):
 
 y = list(map(lambda x, y: [x]*y, [0,1,2], [features[0].shape[0], features[1].shape[0], features[2].shape[0]]))
 mfeatures = mfeatures[1:,:]
+#mfeatures = normalise(mfeatures)
 labels = list(itertools.chain(*y))
 
+k = kimeans(mfeatures, 3)
+labels = k[1]
 
 
-n_neighbors = 15
+n_neighbors = 10
+h = 1000
 
-# import some data to play with
+knn=neighbors.KNeighborsClassifier(10).fit(mfeatures, labels)
+x_min, x_max = mfeatures[:,0].min() - .5, mfeatures[:,0].max() + .5
+y_min, y_max = mfeatures[:,1].min() - .5, mfeatures[:,1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1, figsize=(10, 7))
+plt.set_cmap(plt.cm.Paired)
+plt.pcolormesh(xx, yy, Z)
+
+# Plot also the training points
+plt.scatter(mfeatures[:,0], mfeatures[:,1],c=labels )
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+# import some data to play wfeaturesfeaturesith
 #X = iris.data[:, :2]  # we only take the first two features. We could
                       # avoid this ugly slicing by using a two-dim dataset
 #y = k[1]
 #
-h = 10000  # step size in the mesh
-
-# Create color maps
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-cmap_bold  = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
-
-# we create an instance of Neighbours Classifier and fit the data.
-clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance', n_jobs=-1)
-clf.fit(mfeatures, labels)
-#clf = NearestNeighbors(n_neighbors=15)
-#clf.fit(mfeatures)
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = mfeatures[:, 0].min() - 1, (mfeatures[:, 0].max() + 1)
-y_min, y_max = mfeatures[:, 1].min() - 1, (mfeatures[:, 1].max() + 1)
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                     np.arange(y_min, y_max, h))
-Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-#Z = neigh.kneighbors_graph(mfeatures)
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure()
-plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
-
-Plot also the training points
-plt.scatter(features[:, 0], features[:, 1], c=y, cmap=cmap_bold)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.title("3-Class classification (k = %i, weights = '%s')"
-         % (n_neighbors, 'distance'))
-
-plt.show()
+#h = 10000  # step size in the mesh
+#
+## Create color maps
+#cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+#cmap_bold  = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+#
+## we create an instance of Neighbours Classifier and fit the data.
+#clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance', n_jobs=-1)
+#clf.fit(mfeatures, labels)
+##clf = NearestNeighbors(n_neighbors=15)
+##clf.fit(mfeatures)
+## Plot the decision boundary. For that, we will assign a color to each
+## point in the mesh [x_min, x_max]x[y_min, y_max].
+#x_min, x_max = mfeatures[:, 0].min() - 1, (mfeatures[:, 0].max() + 1)
+#y_min, y_max = mfeatures[:, 1].min() - 1, (mfeatures[:, 1].max() + 1)
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+#                     np.arange(y_min, y_max, h))
+#Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+##Z = neigh.kneighbors_graph(mfeatures)
+## Put the result into a color plot
+#Z = Z.reshape(xx.shape)
+#plt.figure()
+#plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+#
+##Plot also the training points
+##plt.scatter(features[:, 0], features[:, 1], c=y, cmap=cmap_bold)
+#plt.xlim(xx.min(), xx.max())
+#plt.ylim(yy.min(), yy.max())
+#plt.title("3-Class classification (k = %i, weights = '%s')"
+#         % (n_neighbors, 'distance'))
+#
+#plt.show()
